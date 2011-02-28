62c2b41ea23033e05405512178fd8d65684f85dd
Uriel Avalos <uriavalos@yahoo.com>
2011-02-28 16:20:01 -0500

    /home/uri/Documents/Android/InterArt/src/uris/apps/com/PlayGame.java

--- src/uris/apps/com/PlayGame.java
+++ src/uris/apps/com/PlayGame.java
@@ -113,7 +113,7 @@ public class PlayGame extends Activity
 		    }
 
 		    //update pictures
-		    //PlayGame.this.mGameGridView.updateImages();
+		    PlayGame.this.mGameGridView.updateImages();
 
 		    //update progress bar
 		    PlayGame.this.updateProgressBar();
