52478de9ec6fb13513554c17f3de3f208c591478
Uriel Avalos <uriavalos@yahoo.com>
2011-03-10 08:01:39 -0500

    /home/uri/Documents/Android/InterArt/src/uris/apps/com/GoScreen.java

--- src/uris/apps/com/GoScreen.java
+++ src/uris/apps/com/GoScreen.java
@@ -50,7 +50,7 @@ public class GoScreen extends TextView {
 	ringPaint.setStrokeWidth(ringThick);
 	gradPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	gradPaint.setStyle(Paint.Style.STROKE);
-	gradPaint.setStrokWidth(ringThick);
+	gradPaint.setStrokeWidth(ringThick);
     }
     
     @Override
