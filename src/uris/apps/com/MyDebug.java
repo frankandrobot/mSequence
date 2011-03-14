6d1c695b5f7fec4117b736eece62e527fdbe3422
Uriel Avalos <uriavalos@yahoo.com>
2011-03-14 13:33:39 -0400

    /home/uri/projects/InterArt/src/uris/apps/com/MyDebug.java

--- src/uris/apps/com/MyDebug.java
+++ src/uris/apps/com/MyDebug.java
@@ -5,4 +5,8 @@ import android.util.Log
 public class MyDebug {
     static public boolean playGameDebug = true;
     static public boolean scoreLayout= true;
+
+    static public PlayGameLogv(String s) {
+	Log.v("PlayGame", s);
+    }
 }
