49b33e5479df9a9d9d51a38f901357e506b98d7d
Uriel Avalos <uriavalos@yahoo.com>
2011-03-16 13:40:45 -0400

    /home/uri/projects/InterArt/src/uris/apps/com/ProgressBarView.java

--- src/uris/apps/com/ProgressBarView.java
+++ src/uris/apps/com/ProgressBarView.java
@@ -89,7 +89,7 @@ public class ProgressBarView extends GridView {
 	    }
 	}
 	canvas.drawArc(new RectF(px-r,py-r,px+r,py+r), 
-		       0, currentAngle, 
+		       -90, currentAngle, 
 		       false, 
 		       curPaint);
     }
