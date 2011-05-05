42a6fcf431a0c2e5cefa31aa55c305191a804a69
Uriel Avalos <uriavalos@yahoo.com>
2011-05-05 18:27:12 -0400

    Thu May 05 18:27:12 EDT 2011

--- src/uris/apps/com/GameEngine.java
+++ src/uris/apps/com/GameEngine.java
@@ -22,7 +22,9 @@ public class GameEngine {
     //scoring
     boolean[] beenHereBefore;
     int guess_bonus;
-    
+    //clock
+    long timerDuration;
+    long keyPress = 1500; //1.5 seconds
 
     //This creates a GameEngine with no_art_pieces of art pieces, given
     //difficulty, and the given number of stages.
@@ -101,15 +103,23 @@ public class GameEngine {
 	    beenHereBefore[i] = false;
     }
 
+    // Scoring system:
+    //  1.5(no choices)n + 0 + 1.5 + 1.5(2) + ... + 1.5(n-1)
+    // = 1.5(choices)(n) + 1.5(n-1)(n)/2 seconds
+    
+    // where 1.5 is button-press duration, no choices is the number of
+    // choices (for each stage), and n is total number of stages.
     private void initCountdownTimer() {
-	timerDuration = 3000 // 30 seconds
+	timerDuration = keyPress*no_choices_no_stages 
+	    + keyPress*(no_choices-1)*no_choices/2;
+	timerDuration = 30000 // 30 seconds
     }
 
     private void saveSettings() { }	
 
     //start the game
     public void startCountdownTimer() {
-
+	
     }
 
     //playing game, lets...
