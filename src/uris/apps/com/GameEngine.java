7ac910d800479ed3e4e24ab40b7adec308b9d409
Uriel Avalos <uriavalos@yahoo.com>
2011-05-13 19:26:10 -0400

    Fri May 13 19:26:10 EDT 2011

--- src/uris/apps/com/GameEngine.java
+++ src/uris/apps/com/GameEngine.java
@@ -150,7 +150,7 @@ public class GameEngine {
 	return false;
     }
     
-    public boolean reportScores(boolean game_over) {
+    public void reportScores(boolean game_over) {
 	Score.time_bonus = (game_over) ? 0 : (int) mGameClock.getTimeLeft();
 	Score.guess_bonus = guess_bonus * 1000;
     }
