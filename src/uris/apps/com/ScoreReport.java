83e4c72e7e313a4ffdef66b1cfad25b7355ef885
Uriel Avalos <me@frankandbot.com>
2011-05-16 18:27:33 -0400

    Mon May 16 18:27:33 EDT 2011

--- src/uris/apps/com/ScoreReport.java
+++ src/uris/apps/com/ScoreReport.java
@@ -38,7 +38,7 @@ public class ScoreReport extends Activity
 	s[0] = userScores.getIntExtra("currentscore",Score.current_score);
 	s[1] = userScores.getIntExtra("timebonus",Score.time_bonus);
 	s[2] = userScores.getIntExtra("guessbonus",Score.guess_bonus);
-	if ( userScores.getIntExtra("gameover",false) ) {
+	if ( userScores.getIntExtra("gameover",0 ) == InterArt.GAME_OVER ){
 	    setResult(InterArt.GAME_OVER);
 	}
 	scores = (ScoreLayout) findViewById(R.id.scores);
