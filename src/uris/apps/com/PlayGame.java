0b75b5c999932b23b68303e65a85cf4604cd49f5
Uriel Avalos <me@frankandbot.com>
2011-05-18 19:34:51 -0400

    Wed May 18 19:34:51 EDT 2011

--- src/uris/apps/com/PlayGame.java
+++ src/uris/apps/com/PlayGame.java
@@ -233,7 +233,7 @@ public class PlayGame extends Activity
 	// See which child activity is calling us back.
 	switch (requestCode) {
 	case InterArt.GO_SCREEN: {
-	    mGameClock.startCountdownTimer();
+	    mGameClock.restart();
 	}
 	case InterArt.SCORES: {
 	    if ( resultCode == InterArt.GAME_OVER ) {
