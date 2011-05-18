617a66ea210a78d72f1d9b1b33db04ab45503c4d
Uriel Avalos <me@frankandbot.com>
2011-05-18 15:40:27 -0400

    Wed May 18 15:40:27 EDT 2011

--- src/uris/apps/com/Score.java
+++ src/uris/apps/com/Score.java
@@ -1,9 +1,12 @@
 package uris.apps.com;
 
 public class Score {
-    static public int score, current_score, total_score;
-    static public int time_bonus, error_bonus, guess_bonus;
-    static public int incorrect_penal, duplicate_penal;
+    static public int current_score;
+    static public int time_bonus,guess_bonus;
+    static public int total_score;
+    //static public int score, current_score, total_score;
+    //static public int time_bonus, error_bonus, guess_bonus;
+    //static public int incorrect_penal, duplicate_penal;
     
     static public String mytoString() {
 	String tmp;
@@ -14,4 +17,10 @@ public class Score {
 	tmp += "  dup:" + duplicate_penal;
 	return tmp;
     }
+
+   static public void reportScores(boolean isLevelOver, int timeLeft) {
+	Score.time_bonus = (isLevelOver) ? 0 : timeLeft;
+	Score.guess_bonus = guess_bonus * 1000;
+    }
+
 }
